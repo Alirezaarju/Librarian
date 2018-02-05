e27a67d225ad2bd7b571a3cb8a989e22d8eb4c94
Ali Reza Arju <alirezaarju@gmail.com>
2018-02-04 19:36:08 -0800

    Assignment5

--- librarian.js
+++ librarian.js
@@ -3,28 +3,29 @@ var library = [
   { title: "The Old Man and the Sea", year: "1952", author: "Ernest Hemingway", publisher: "Charles Scribner's Sons" },
   { title: "Mythos: A Retelling of the Myths of Ancient Greece", year: "1981", author: "Stephen Fry", publisher: "Penguin" },
   { title: "The Mythical Man-Month", year: "1975", author: "Fred Brooks", publisher: "Addison-Wesley" },
-  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" }
-  /* Task 2 */
+  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" },
+  { title: "Emma",year: "1816", author:"Jane Austen",publisher: " John Murray "}
 ];
 
 /* Task 1 */
 function getBookTitle( index ){
-  //add code
+return library[index].title;
 }
 
-console.log(getBookTitle(3));
+console.log(getBookTitle(5));
 
 /* Task 3 */
 function addBook( book ){
-  //add code
+library.push(book);
+ return library;
 }
 
-var newBook = {};
+var newBook = { title: "A", year: "1999", author: "B", publisher: "C" };
 console.log(addBook(newBook));
 
 /* Task 4 */
 function bookByAuthor(){
-  //add code
+
 }
 
 console.log(bookByAuthor());
